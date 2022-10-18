61a6a655043a118634cf4c47faeef9a745c0f9d1
Brady Yanoff <113472249+Bradyyanoff@users.noreply.github.com>
2022-10-18 12:04:17 -0400

    writing the number of words that show up

---
+++ inclassproject.py
@@ -0,0 +1,14 @@
+word_file = open("www.gutenberg.org.txt", 'r')
+word_lines = word_file.readlines()
+word_counts = {'the': 0}
+for word_line in word_lines:
+    list_of_words = word_line.split(" ")
+    for current_word in list_of_words:
+        current_word = current_word.lower()
+        current_word = current_word.strip()
+        if word_counts.get(current_word):
+           word_counts[current_word] += 1
+           #same as word_counts[current_word] = word_counts[current_word]+1
+        else:
+            word_counts[current_word] = 1
+print(word_counts)
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 word_file = open("WordFile.txt", 'r')
 word_lines = word_file.readlines()
+word_counts = {'the': 0}
 for word_line in word_lines:
     list_of_words = word_line.split(" ")
-    print(list_of_words)
+    for current_word in list_of_words:
+        current_word = current_word.lower()
+        current_word = current_word.strip()
+        if word_counts.get(current_word):
+            word_counts[current_word] += 1
+            # same as word_counts[current_word] = word_counts[current_word]+1
+        else:
+            word_counts[current_word] = 1
+print(word_counts)
