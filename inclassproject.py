80097395bcc94da44c1622bef7487755a31d7df3
Brady Yanoff <113472249+Bradyyanoff@users.noreply.github.com>
2022-10-20 11:01:28 -0400

    writing the number of words that show up

--- inclassproject.py
+++ inclassproject.py
@@ -7,17 +7,9 @@ for word_line in word_lines:
         current_word = current_word.lower()
         current_word = current_word.strip()
         if word_counts.get(current_word):
-           word_counts[current_word] += 1
+            word_counts[current_word] += 1
            #same as word_counts[current_word] = word_counts[current_word]+1
         else:
-            word_counts = {'the': 0}
-            for word_line in word_lines:
-                list_of_words = word_line.split(" ")
-                for current_word in list_of_words:
-                    current_word = current_word.lower()
-                    current_word = current_word.strip()
-                    if word_counts.get(current_word):
-                        word_counts[current_word] += 1
+            word_counts[current_word] = 1
 for word, count in word_counts.items():
     print(f"{word}:  \t {count}")
-
